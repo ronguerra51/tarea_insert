f5a35adc5391688633daf999cbd813ca96d645c5
Roger <ronguerra51@gmail.com>
2024-05-30 09:22:03 -0600

    se redirecciono en el navbar clientes y se agrego bien la direccion del cliente

--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar fixed-top navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../clientes/index.php"><i class="bi bi-file-person"></i>CLIENTES</a>
+    <a class="navbar-brand" href="../../vistas/clientes/index.php"><i class="bi bi-file-person"></i>CLIENTES</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"><i class="<bi bi-file-person"></i>INICIO</a>
+          <a class="nav-link active" aria-current="page" href="../../vistas/clientes/index.php"><i class="<bi bi-file-person"></i>INICIO</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
